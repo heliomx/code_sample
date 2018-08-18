a48a91c41b0bb107cb11a820486c7ac45bce395d
Hélio Miranda <heliomx@gmail.com>
2018-08-18 11:17:24 -0300

    icons

--- resources/views/public.blade.php
+++ resources/views/public.blade.php
@@ -5,6 +5,7 @@
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>Rádio Estúdio Brasil</title>
     <link href="https://fonts.googleapis.com/css?family=Titillium+Web:300,400,700" rel="stylesheet">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
 </head>
 <body>
     <div>
